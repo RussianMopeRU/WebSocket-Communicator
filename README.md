277bfe4149bd43c63fd6b0377b6fabea1d1dcb18
RussianMope <74246343+RussianMopeRU@users.noreply.github.com>
2022-04-28 15:27:31 +0300

    Upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # WebSocket-Communicator
+# Allows sending websocket messages without a websocket variable, can show what the client sends to the server and the server sends to the client. [USE IN TAMPERMONKEY]
---
+++ script.js
@@ -0,0 +1,50 @@
+// ==UserScript==
+// @name             WebSocket Communicator
+// @author           RussianMope[RU]
+// @include          *://*
+// @run-at           document-start
+// @grant            none
+// ==/UserScript==
+
+const sockets = [];
+
+const ws = window.WebSocket;
+window.WebSocket.prototype.fakeSend = window.WebSocket.prototype.send;
+
+window.WebSocket.prototype.send = function() {
+    this.fakeSend.apply(this, arguments);
+    console.debug(arguments[0]); // what client sents
+}
+
+window.WebSocket = function(...args){
+  const socket = new ws(...args);
+  sockets.push(socket);
+
+  return socket;
+};
+
+function updSockets(){
+window.sockets = sockets;
+requestAnimationFrame(updSockets)
+}
+
+function handleWsMsg(message){
+    // console.debug(message.data) // what client gets
+}
+
+function getValidSocket(){
+    sockets.forEach(socket => {
+        if(socket.readyState == 1)  {
+            window.validSocket = socket; // USAGE ---> window.validSocket.send('hello')
+         socket.addEventListener("message", (mes) => {
+             handleWsMsg(mes)
+         });
+        }
+    })
+}
+
+window.getValidSocket = getValidSocket;
+
+setInterval(getValidSocket, 1000)
+
+requestAnimationFrame(updSockets);
